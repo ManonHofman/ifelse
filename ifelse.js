7594d97f389083bca2be938a0307227950a0bc21
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-22 13:53:42 +0200

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,22 @@
+var myAge = 25;
+
+if (myAge > 17) {
+    document.write ("Je mag naar binnen!")
+}
+if (myAge < 18) {
+    document.write ("Ik verzoek je om naar buiten te gaan!")
+}
+
+var isFemale = true;
+if (isFemale){
+    document.write ("Jij bent een vrouw")
+}
+else{
+    document.write ("jij bent een man")
+}
+
+var driverStatus = bob;
+if (driverStatus = bob){
+    docuement.write("Jij mag rijden!")
+}
+else {document.write ("Jij mag niet rijden")}
